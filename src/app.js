476f8f957e64ce6f10b0b8ca700414e905268517
jackbaso <jbaso507@gmail.com>
2022-03-26 22:01:11 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["our", "my", "your"];
+let adj = ["great", "super", "best"];
+let noun = ["frog", "lion", "jack"];
+let ext = [".com", ".net"];
+
+function generadorDominios() {
+  var domains = [];
+
+  for (let pro of pronoun) {
+    for (let adjetivo of adj) {
+      for (let noun of noun) {
+        ext.forEach(ext => {
+          domains.push(pro + adjetivo + noun + ext);
+        });
+      }
+    }
+  }
+  return domains;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(generadorDominios());
 };
